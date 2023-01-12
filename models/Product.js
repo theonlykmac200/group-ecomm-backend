fb78f9d56b5c55068689906558919e4345cde1a8
unknown <kodytran854@gmail.com>
2023-01-12 01:41:41 -0500

    fixed models

--- models/Product.js
+++ models/Product.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose")
 const ProductSchema = new mongoose.Schema({
   Title: { type: String, required: true },
   Price: { type: Number, required: true },
-  description: { type: String, required: true },
-  quantity:{type:Number,require:true},
-  
+  desc: { type: String, required: true },
+  qty:{type:Number,require:true},
+  img:{type: String, required: true}
 })
 
 const Product = mongoose.model("Product", ProductSchema)
