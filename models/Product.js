9acd735c13121455a846d840792f140b1a654156
unknown <kodytran854@gmail.com>
2023-01-12 01:34:22 -0500

    added models

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const ProductSchema = new mongoose.Schema({
   Title: { type: String, required: true },
   Price: { type: Number, required: true },
-  discription: { type: String, required: true },
+  description: { type: String, required: true },
   quantity:{type:Number,require:true},
   
 })
